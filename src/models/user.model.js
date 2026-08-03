16f92a0639b5110e94e6d47abccdc698edd5bd92
Lakshay kapoor <lakshkpr29@gmail.com>
2026-08-03 21:23:28 +0530

    cloudinary uploading

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema= new Schema (
     {
@@ -10,6 +12,11 @@ const userSchema= new Schema (
             trim: true,
             index: true,
             unique: true
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
         },
         email: {
             type: String,
@@ -43,5 +50,48 @@ const userSchema= new Schema (
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function (next) {
+
+    /// now checking that whether password is updated or saced or not
+    if(!this.ismodified("password")) return next()
+
+        ////else trigger the below statements
+    this.password= bcrypt.hash(this.password,10);
+    next()
+}),
+
+userSchema.methods.isPasswordCorrect= async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken= async function() {
+    jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken= async function() {
+    jwt.sign(
+        {
+            _id: this._id,
+            // the diff. b/w refresh token and access token is also that
+            //  refresh tokn does not contain much info. about the user
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User= mongoose.model('User', userSchema)
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs"
+
+// v2 is a kind of built in liabrary that contains uploading inbuilt fuction to upload the req. file
+// this "fs" is cxalled a file system that handles or take under control the file handling
+// this fs is by default added with "node js"
+// helps to handle file/ edit it/ open it/ read it/ collect data from it etc.
+// "unlink" means deleting a file i.e unlinking it from the file system
+
+
+const uploadingOnCloudinary= async function(localFilePath) {
+    try {
+        if(!localFilePath) return null
+        // we are gonna upload the local file path to cloudinary
+        const response= await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        /// from now if file has been uploaded successfully then
+        console.log("file uploaded successfully", response.url)
+        return response.url
+        /// we have returned response to the user so that it can extract url req from it
+        
+    } catch (error) {
+        /// if local file path jasn't been uploaded to cloudinary then
+         fs.unlinkSync(localFilePath)
+         ///here is for uploading operation failed
+         return null
+    }
+}
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
